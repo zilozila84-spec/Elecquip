b5aae6b44a659d34389a15e517b6aea76472051f
zilozila84-spec <zilozila84@gmail.com>
2026-01-19 13:03:56 +0200

    Update app.js

--- learning/app.js
+++ learning/app.js
@@ -1,17 +1,3 @@
 function loginStudent() {
-  const studentNumber = document.getElementById("loginNumber").value.trim();
-  const password = document.getElementById("loginPassword").value.trim();
-
-  if (studentNumber === "" || password === "") {
-    alert("Please enter your student number and password");
-    return;
-  }
-
-  // Save student data (client-side)
-  localStorage.setItem("elecquip_student_number", studentNumber);
-  localStorage.setItem("elecquip_logged_in", "yes");
-
-  // Redirect to dashboard
-  window.location.href = "dashboard.html";
+  alert("Login button works");
 }
-
